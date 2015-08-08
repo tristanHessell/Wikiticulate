200c804d4faa64cdc83bd3b951fa5b47a99056aa
Jake Sebastian-Jones <trideceth12@gmail.com>
2015-08-08 17:57:06 +1000

    Added custom application class for storing application wide state.

---
+++ app/src/main/java/tristan/hessell/pizza/wikiticulate/app/WikitulateApplication.java
@@ -0,0 +1,13 @@
+package tristan.hessell.pizza.wikiticulate.app;
+
+import android.app.Application;
+
+/**
+ * Created by manko on 8/08/15.
+ */
+public class WikitulateApplication extends Application {
+
+    WikitulateApplication() {
+        super();
+    }
+}
