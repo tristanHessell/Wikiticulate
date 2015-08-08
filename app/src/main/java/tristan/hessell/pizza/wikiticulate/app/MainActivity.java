71440a16d5bb15720510891b519f324b9fb82a48
Jake Sebastian-Jones <trideceth12@gmail.com>
2015-08-08 21:28:19 +1000

    Added next and pass buttons

--- app/src/main/java/tristan/hessell/pizza/wikiticulate/app/MainActivity.java
+++ app/src/main/java/tristan/hessell/pizza/wikiticulate/app/MainActivity.java
@@ -5,11 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-    private int numPlayers;
+    private int      mNumPlayers;
+    private TextView mArticleText;
+    private WikitulateApplication mApplication;
 
     @Override
     protected void onCreate( Bundle savedInstanceState )
@@ -18,21 +22,66 @@ public class MainActivity extends AppCompatActivity
         setContentView( R.layout.activity_main );
 
 
-        numPlayers = getIntent().getIntExtra( "numPlayers", -1 );
-        //if(numPlayers == -1)
+        mNumPlayers = getIntent().getIntExtra( "numPlayers", -1 );
+        //if(mNumPlayers == -1)
         //{
             //TODO cry as the number of players wasnt set.
         //}
 
-        WikitulateApplication application = (WikitulateApplication) getApplication();
+        mApplication = (WikitulateApplication) getApplication();
+
+        mArticleText = (TextView) findViewById(R.id.articleTitleText);
 
         Log.d("MainActivity",
-                application.getArticleCount() + " articles available after onCreate()");
+                mApplication.getArticleCount() + " articles available after onCreate()");
 
-        if(application.getArticleCount() > 0)
+        if(mApplication.getArticleCount() > 0)
         {
-            TextView text = (TextView) findViewById(R.id.articleTitleText);
-            text.setText(application.getNextArticle());
+            mArticleText.setText(mApplication.getNextArticle());
+        }
+    }
+
+    private void startRound() {
+        mArticleText.setVisibility(View.VISIBLE);
+        Button passButton = (Button) findViewById(R.id.passButton);
+        Button nextButton = (Button) findViewById(R.id.nextButton);
+        Button startButton = (Button) findViewById(R.id.startButton);
+
+        passButton.setVisibility(View.VISIBLE);
+        nextButton.setVisibility(View.VISIBLE);
+        startButton.setVisibility(View.GONE);
+
+    }
+
+    private void pass() {
+        if(mApplication.getArticleCount() > 0)
+        {
+            mArticleText.setText(mApplication.getNextArticle());
+        }
+    }
+
+    private void next() {
+        if(mApplication.getArticleCount() > 0)
+        {
+            mArticleText.setText(mApplication.getNextArticle());
+        }
+    }
+
+    public void onNextBtnClicked(View v){
+        if(v.getId() == R.id.nextButton){
+            next();
+        }
+    }
+
+    public void onPassBtnClicked(View v){
+        if(v.getId() == R.id.passButton){
+            pass();
+        }
+    }
+
+    public void onStartBtnClicked(View v){
+        if(v.getId() == R.id.startButton){
+            startRound();
         }
     }
 
